3d30263c55994ce8cdd5b16b441804a025376caf
shashank-indukuri <shashank.indukuri05@gmail.com>
2022-04-12 13:33:36 -0500

    Updated the main

--- src/main/java/com/test/cicd/testspringboot/TestspringbootApplication.java
+++ src/main/java/com/test/cicd/testspringboot/TestspringbootApplication.java
@@ -2,6 +2,7 @@ package com.test.cicd.testspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class TestspringbootApplication {
 	@GetMapping("/version")
 	public String version() {
 		return "The current version is 0.0.2";
+	}
+
+	@Bean
+	public void display(){
+		System.out.println("Hello world");
 	}
 
 }
